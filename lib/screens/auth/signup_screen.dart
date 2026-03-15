4336e802b9634e1e08dfc6a1c7607e8c01ace497
ocean <ocean.tqhc@gmail.com>
2026-03-15 11:31:06 +0530

    Implemented signup screen with Firebase authentication

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,13 +1,82 @@
 import 'package:flutter/material.dart';
+import '../../services/auth_service.dart';
+import '../main_screen.dart';
 
-class SignupScreen extends StatelessWidget {
-
-  const SignupScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  SignupScreen({super.key});
 
   @override
+  State<SignupScreen> createState() => _SignupScreenState();
+
+}
+class _SignupScreenState extends State<SignupScreen> {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final AuthService authService = AuthService();
+
+  Future<void> signupUser() async {
+    final email = emailController.text.trim();
+    final password = passwordController.text.trim();
+
+    final user = await authService.signUp(email, password);
+
+    if (user != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainScreen(),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Signup failed")),
+      );
+    }
+  }
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Signup")),
+
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+
+        child: Column(
+          children: [
+
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Password",
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: signupUser,
+              child: const Text("Create Account"),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
